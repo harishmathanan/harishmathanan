4cca980d7c2ea7e5a8fa7506b9d59869f5b99bef
Harish Mathanan <harish.mathanan@hotmail.com>
2020-04-19 17:07:52 +0800

    Setup configuration on eleventy.js file

---
+++ eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(config) {
+
+  // returns all posts
+  config.addCollection('post', collection => {
+    return collection.getFilteredByGlob('posts/*.md')
+  });
+
+  // returns the last 5 posts
+  config.addCollection('home', collection => {
+    const arr = collection.getFilteredByGlob('posts/*.md')
+    return arr.slice(Math.max(arr.length - 5, 0))  
+  });
+
+  config.addPassthroughCopy("img");
+  config.addPassthroughCopy("css");
+};
